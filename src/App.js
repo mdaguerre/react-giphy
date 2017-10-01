da22782660e35734fced54b1f158a259c911bf32
Martin Daguerre <mdaguerre@gmail.com>
2017-09-30 21:49:23 -0300

    Add search and ux improve

--- src/App.js
+++ src/App.js
@@ -2,15 +2,40 @@ import React, { Component } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import GiphyApi from './Services/api';
+import {DebounceInput} from 'react-debounce-input';
+
 
 class App extends Component {
 
   state = {
-    giphs: []
+    giphs: [],
+    pagination: {
+      count: 0,
+      offset: 0,
+      total_count: 0
+    }
   }
+
+  constructor(props) {
+    super(props);
+    this.handleSearch = this.handleSearch.bind(this);   
+  }
+
   componentDidMount(){
     GiphyApi.getTrending().then((response) => {
-      this.setState({giphs : response.data})
+      this.setState({giphs : response.data, pagination: response.pagination})
+    });
+  }
+
+  handleSearch(e){
+    let q = e.target.value;
+    this.search(q);
+  }
+
+  search(q){
+    let offset = this.state.pagination.offset;
+    GiphyApi.searchGifs(q, offset).then((response) => {
+      this.setState({giphs : response.data, pagination: response.pagination})
     });
   }
 
@@ -20,34 +45,63 @@ class App extends Component {
          <div className="container-fluid">
             <div className="starter-template">
               <h4>Search into giphy</h4>
-              <SearchInput />
+              <SearchInput onKeyUp={this.handleSearch}/>
             </div>
-            <ResultsContainer giphs={this.state.giphs}/>
+            <ResultsContainer giphs={this.state.giphs} pagination={this.state.pagination} />
           </div>
       </Router>
     );
   }
 }
 
-const SearchInput = () => (
-  <input type="text" className="form-control search-input" />  
+const SearchInput = (props) => (
+  <DebounceInput className="form-control search-input" onChange={props.onKeyUp} debounceTimeout={300} />
 );
 
-const ResultsContainer = (props) => (
-  <div className="results-container">
-   <Results giphs={props.giphs}/>
-  </div>
-);
+const ResultsContainer = (props) => {
+  let content = <Results giphs={props.giphs} />;
+
+  if(props.giphs.length == 0){
+    content = <Giph className="center" url="https://media.giphy.com/media/l41lI4bYmcsPJX9Go/giphy.gif" />;    
+  }
+
+  return(
+    <div className="results-container">
+      <Pager pagination={props.pagination} />
+      {content}
+    </div>
+  )
+};
 
 const Results = (props) => (
   <ul className="results-list">
-     {props.giphs.map( giph => <li><Giph url={giph.images.fixed_height.url} /></li> )}
+     {props.giphs.map( (giph) => {
+       
+       if(!giph.images.preview_gif){
+         console.warn('giph broken ', giph);
+         return;
+       }
+
+       return(
+          <li key={giph.id} ><Giph url={giph.images.preview_gif.url} key={giph.id} /></li>
+        )
+       }
+     )}
   </ul>
 );
 
-const Giph = (props) => (
-  <img src={props.url} className="rounded" alt="..." />
-)
+const Giph = (props) => {
+  const c = props.className || '';  
+  return (
+    <img src={props.url} className={`rounded ${c}`} alt="..." />
+  )
+}
+const Pager = (props) => {
+  console.log(props.pagination);
 
+  return (
+    <div></div>
+  );
+}
 
 export default App;
--- src/Services/api.js
+++ src/Services/api.js
@@ -10,6 +10,15 @@ class GiphyApi {
           console.log('parsing failed', ex)
         })
     }
+
+    static searchGifs(q, offset){
+        return fetch(`${HOST}/v1/gifs/search?q=${q}&offset=${offset}&api_key=${API_KEY}`)
+        .then((response) => {
+            return response.json();
+        }).catch((ex) => {
+          console.log('parsing failed', ex)
+        })
+    }
 }
 
 export default GiphyApi;
